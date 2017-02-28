19e139feb15f640370dd101a6a6a5b5d4a833a8c
sugihp14 <sugihpurnama14@gmail.com>
2017-02-28 16:50:49 +0700

    versi 1.0.0

---
+++ Pertemuan-2/Mahasiswa.java
@@ -0,0 +1,7 @@
+public class Mahasiswa{
+  public static void main(String[] args){
+	System.out.println("Nama Lengkap	: Sugih Purnama");
+	System.out.println("Nim		: 10115478");
+	System.out.println("Alamat		: Bandung");
+  }
+}
