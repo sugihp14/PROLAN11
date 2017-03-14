4e9cf6bbe79241e04d1cdcd134ac11c1739c5001
Sugih Purnama <sugihpurnama14@gmail.com>
2017-03-14 22:38:20 +0700

    10115478

---
+++ PERTEMUAN-4(Method, this,Konstruktor,Atribut)/IsiData.java
@@ -0,0 +1,17 @@
+
+
+public class IsiData {
+    String Nama;
+    String Nim;
+
+ public void Data(String Nama, String Nim){
+        this.Nama=Nama;
+        this.Nim=Nim;
+        System.out.print("Terima Kasih ");
+        System.out.println(Nama);
+        
+ }
+
+   
+    
+}
---
+++ PERTEMUAN-4(Method, this,Konstruktor,Atribut)/Kalkulator.java
@@ -0,0 +1,38 @@
+
+
+public class Kalkulator{
+ private int a,b;
+  public int Penjumlahan(int a,int b){
+	
+ 
+	return a + b;
+
+	}
+public int Pengurangan(int a, int b){
+	
+ 	
+	return a - b;
+
+	}	
+
+ public int Perkalian(int a, int b){
+	
+	return a * b;
+
+	
+
+	}	
+ 
+ public float Pembagian(float a,float b){
+	
+	
+	return a / b;
+
+	
+	}	
+
+ 
+    
+
+ 
+}
