e4b52dcba9890319d49a239a02a15634c11ed145
Sugih Purnama <sugihpurnama14@gmail.com>
2017-07-17 19:45:01 +0700

    update

---
+++ Pertemuan-12/login.java
@@ -0,0 +1,23 @@
+
+public class login<T> {
+    private T login;
+  
+    
+    
+    
+    public void setLogin(T login){
+        this.login=login;
+        
+    }
+    
+    public T getLogin(){
+            return (login);
+           
+      
+            
+    
+    }
+    
+    
+    
+}
